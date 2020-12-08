0eff24658251eeee289e17369b1dca9cdc5e80b8
koola <coolenovic@hotmail.com>
2020-12-08 20:14:11 +0100

    added two useEffects, one for when index is out of reach to reset, second is for sliding using setInterval

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -6,6 +6,23 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length -1;
+    if(index < 0){
+      setIndex(lastIndex)
+    }
+    if(index > lastIndex){
+      setIndex(0)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index+1)
+    }, 3000);
+    return () => clearInterval(slider)
+  }, [index])
+
   return (
     <section className="section">
       <div className="title">
@@ -38,11 +55,11 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index-1)}>
           <FaChevronLeft />
         </button>
 
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index+1)}>
           <FaChevronRight />
         </button>
       </div>
