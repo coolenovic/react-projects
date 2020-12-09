96779729faf57cef78b9e9caf0903f2bf21e06df
koola <coolenovic@hotmail.com>
2020-12-09 13:37:35 +0100

    added paragraph display functionality

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -6,7 +6,17 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+    let amount = parseInt(count)
+
+    if(count <= 0){
+      amount =1
+    }
+
+    if(count > 8){
+      amount = 8
+    }
+
+    setText(data.slice(0,amount))
   }
   
   return (
@@ -21,8 +31,9 @@ function App() {
       <button type="submit" className="btn">generate</button>
     </form>
     <article className="lorem-text">
-      <p>paragraph 1</p>
-      <p>paragraph 2</p>
+      {text.map((item, index) => {
+        return <p key={index}>{item}</p>
+      })}
     </article>
   </section>
     )
